a9fffa8fb428588f6f618c1a067916abb094423a
jangramonikaa-web <jangramonikaa@gmail.com>
2026-07-06 23:03:38 +0530

    create task2.py

---
+++ task2.py
@@ -0,0 +1,13 @@
+# ai engineer pofile
+ai_engineer={
+    "name":"monika",
+    "company":"tcs",
+    "experience": 2,
+    "salary": 50000.00,
+    "skills":["python","ai","data science"],
+    "programming_languages" :["python","java","c++"],
+    "projects":{"project1":"ai chatbot","project2":"image recognition"},
+    "certifications":{"certification1":"ai engineer","certification2":"data science"},
+    "available_for_work": True
+}
+print((ai_engineer))
