0ccc736376515ae67f3b8d347ea3d30701ec703c
jangramonikaa-web <jangramonikaa@gmail.com>
2026-07-06 18:13:34 +0530

    create variable

---
+++ datatype.py
@@ -0,0 +1,15 @@
+# Create variables for
+# Your age
+# Number of students in a class
+# Number of books
+# Number of mobile phones in your house
+ 
+age = 25  # Your age
+no_of_students = 44  # Number of students in a class
+no_of_books = 102  # Number of books
+no_of_mobile_phones = 3  # Number of mobile phones in your house
+
+print("Age:", age)
+print("Number of students in a class:", no_of_students)
+print("Number of books:", no_of_books)  
+print("Number of mobile phones in your house:", no_of_mobile_phones)
