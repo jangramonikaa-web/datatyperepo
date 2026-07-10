82b7dbf2db0cf3c7d98d5e877b30d4d08346e621
jangramonikaa-web <jangramonikaa@gmail.com>
2026-07-10 13:02:14 +0530

    practice

---
+++ task4.py
@@ -0,0 +1,15 @@
+marks=50
+greater=True
+if(marks<=40):
+    if( marks=50):
+     print("passed with c grade")
+    else:
+       print("failed")
+
+    # if(marks<80):
+    #    print("pass with b grade")
+    # else:
+    #    print("fail")
+
+else:
+   print("not found")
---
+++ tempCodeRunnerFile.py
@@ -0,0 +1,27 @@
+# # tasks of python learning fo practice
+name="monika"
+age=20
+course= "bca"
+city="gurgeon"
+percentage=98.4
+print(name,age,course,city,percentage)
+
+# personal details
+name="monika"
+father_name="mr. surrender"
+mother_name="mrs. geeta"
+phone=9050194517
+email="jangramonikaagmail.com"
+print(name)
+print(father_name)
+print(mother_name)
+print(phone)
+print(email)
+#   guess the number
+secret_number = 7
+guess=0
+
+while guess != secret_number:
+    guess = int(input("guess the number"))
+
+print("corect!")
